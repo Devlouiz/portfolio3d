28faa8e04522ceb6351b9e873cd50dd3e5dbe132
devlouis <louisonyedi@gmail.com>
2023-10-22 17:42:52 +0100

    after my tech stack

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -5,6 +5,11 @@ import { Decal, Float, OrbitControls, Preload, useTexture } from "@react-three/d
 import { technologies } from "../constants";
 import { SectionWrapper } from "../hoc";
 
+import allIcons from "simple-icons";
+import { v4 } from "uuid";
+import { IconCloud } from "react-icon-cloud";
+import { styles } from "../styles";
+
 const Ball = (props) =>{
   const [decal] = useTexture([props.imgUrl])
   return(
@@ -43,8 +48,35 @@ const BallCanvas =({icon})=>{
 }
 
 const Tech = () => {
+  const tagCanvasOptions = {
+    imageScale: 2,
+    initial: [0.1, -0.1],
+    reverse: true,
+    tooltip: "native",
+    tooltipDelay: 0,
+    wheelZoom:  true
+  }
+  const iconSlugs = [
+    "wordpress","nextdotjs","visualstudiocode","tailwindcss","react","html5","javascript",
+    "github","git","figma","docker"
+  ];
+  const iconTags = iconSlugs.map((slug) => ({
+    id: slug,
+    simpleIcon: allIcons.Get(slug)
+  }))
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-3
+    <div className="flex flex-col items-center justify-center">
+      <h2 className={styles.heroHeadText}>Tech Stack</h2>
+      <IconCloud
+      key={v4()}
+      id={"icon"}
+      minContrastRatio={1}
+      iconSize={150}
+      backgroundHexColor={"#000"}
+      fallbackHexColor={"#fff"}
+      tags={iconTags}/>
+    </div>
+    /*<div className="flex flex-row flex-wrap justify-center gap-3
     w-[80%] mx-auto">
       {technologies.map((tech) => (
         <div className="w-40px h-65px">
@@ -52,6 +84,17 @@ const Tech = () => {
         </div>
       ))}
     </div>
+    <div>
+      <IconCloud
+      key={v4()}
+      id={"icon"}
+      minContrastRatio={1}
+      iconSize={50}
+      tags={iconTags}
+      tagCanvasOptions={tagCanvasOption}
+      backgroundHexColor={"#fff"}
+      fallbackHexColor={"#000"}/>
+    </div>*/
   )
 }
 export default SectionWrapper(Tech, 'Tech')
